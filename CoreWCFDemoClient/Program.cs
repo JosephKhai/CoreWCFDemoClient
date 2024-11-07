9a3846e5b2bbafde12d4c85a2ed715a17317e197
Joseph Sektak <Joseph.Sektak@pentanasolutions.com>
2024-11-07 14:06:56 +1100

    Add project files.

---
+++ CoreWCFDemoClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using ServiceReference1;
+
+namespace CoreWCFDemoClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WCF().GetAwaiter().GetResult();
+        }
+
+
+        public static async Task WCF()
+        {
+            while (true)
+            {
+                var client = new EchoServiceClient(EchoServiceClient.EndpointConfiguration.BasicHttpBinding_IEchoService, "http://localhost:6000/EchoService/basichttp");
+
+                //Console.WriteLine("Enter a message!");
+                //string input = Console.ReadLine();
+
+                //var simpleResult = await client.EchoAsync(input);
+
+
+                Console.WriteLine("Enter a message!");
+                string input2 = Console.ReadLine();
+
+                var msg = new EchoMessage()
+                {
+                    Text = input2
+                };
+
+                var msgResult = await client.ComplexEchoAsync(msg);
+            }
+
+        }
+    }
+}
